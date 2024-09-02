77cd8006f15a4cf4a00d76dba2447cac32309b70
Lejitop <alejocrrs@gmail.com>
2024-09-01 22:05:51 -0500

    chore: Refactor server startup and database connection code

--- index.ts
+++ index.ts
@@ -1,38 +1,37 @@
 import app from "@/app";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 const PORT = parseInt(process.env.PORT ?? "3000");
 
-// Función para establecer el puerto del servidor o dinamicante cambiar el puerto por defecto si este esta ocupado 
 const startServer = (port: number) => {
-  app.listen(port, () => {
-    console.log(`Servidor corriendo correctamente en la url: localhost:${port}`);
-  }).on('error', (err: NodeJS.ErrnoException) => {
-    if (err.code === 'EADDRINUSE') {
-      console.log(`El puerto ${port} está en uso, intentando con otro puerto...`);
-      startServer(port + 1); // Intentar con el siguiente puerto
-    } else {
-      console.error('Error al iniciar el servidor:', err);
-    }
-  });
+  app
+    .listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    })
+    .on("error", (err: NodeJS.ErrnoException) => {
+      if (err.code === "EADDRINUSE") {
+        console.log(
+          `The port ${port} is already in use, trying the next one...`
+        );
+        startServer(port + 1); // Try the next port
+      } else {
+        console.log("An error occurred while trying to start the server", err);
+      }
+    });
 };
 
 
-//realizar la conexión con la base de datos en el cloud de mongo
-var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-
 mongoose
   .connect(
     `mongodb+srv://${process.env.user}:${process.env.pass}@${process.env.cluster}.mongodb.net/${process.env.database}?retryWrites=true&w=majority`
   )
   .then(() => {
-    console.log("Conexión con la base de datos establecida con exito..");
+    console.log("Connected to the database successfully");
     startServer(PORT);
   })
   .catch((err: any) => {
-    console.log("Ha ocurrido un error"), console.log(err);
+    console.error("Error connecting to the database", err);
   });
-
-
--- src/app.ts
+++ src/app.ts
@@ -2,29 +2,32 @@ import express, { Application } from "express";
 import Routes from "@routes/index";
 import cors from "cors";
 const app: Application = express();
-// Lista de orígenes permitidos desde .env
-const whitelist = process.env.WHITELIST
-    ? process.env.WHITELIST.split(",")
-    : ["*"];
 
-// Configuración de CORS
+// CORS whitelist
+const whitelist = process.env.WHITELIST
+  ? process.env.WHITELIST.split(",")
+  : ["*"];
+
+// CORS configuration
 const corsOptions = {
-    origin: function (origin: string | undefined, callback: Function) {
-        if (origin === undefined) return callback(null, true);
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            // Permitir si el origen está en la lista o si la solicitud no tiene origen (por ejemplo, solicitudes desde el mismo origen)
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+  origin: function (origin: string | undefined, callback: Function) {
+    if (origin === undefined) return callback(null, true);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      // If the origin is in the whitelist or if it is undefined (same origin), allow the request
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 };
-// MIDDLEWARE
+
+// Middlewares
 app.set("trust proxy", true);
 app.use(express.json());
-app.use(cors(corsOptions)); // Usar CORS con la configuración personalizada
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
-// IMPORTAR ROUTER
+
+// Use the routes
 const routes = new Routes();
 app.use("/api", routes.getRouter);
 
